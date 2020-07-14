cdb80337daa7cff660719a04b9b398a39c55d6c3
Standa Zitta <stanislav.zitta@ronalgroup.com>
2020-07-14 08:37:54 +0200

    version 1

--- parser.py
+++ parser.py
@@ -8,17 +8,18 @@ result_json = (result.json())
 my_ip_set = set()
 for i in range(0,(len(result_json))):
     try:
-        #print(result_json[i]['urls'])
-        pass
+        current_urls = result_json[i]['urls']
+        for current_url in current_urls:
+            if(current_url.find('365' ) > -1 or current_url.find('outlook')>-1) :
+                print(current_url)
+                for q in range(0, len(result_json[i]['ips'])):
+
+                    if '.' in result_json[i]['ips'][q]:
+                        my_ip_set.add(result_json[i]['ips'][q])
+                        print(result_json[i]['ips'][q])
     except:
         continue
-    try:
-        for q in range( 0, len(result_json[i]['ips']) ):
-                #print( result_json[i]['ips'][q] )
-                if '.' in result_json[i]['ips'][q]:
-                    my_ip_set.add(result_json[i]['ips'][q])
-    except:
-        continue
+print('-----------------------------------')
 #print(my_ip_set)
 for net in my_ip_set:
     print(net)
